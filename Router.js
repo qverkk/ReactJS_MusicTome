0333bc6958b60fb1493e96c1c966ba92550c5aa8
filip szeszko <filip@biznesport.pl>
2020-06-07 16:50:37 +0200

    initial commit

---
+++ Router.js
@@ -0,0 +1,176 @@
+const bcrypt = require('bcrypt');
+
+class Router {
+    
+    constructor(app, db) {
+        this.login(app,db);
+        this.logout(app, db);
+        this.isLoggedIn(app, db);
+        this.register(app,db);
+    }
+
+    register(app,db) {
+        app.post('/register', (req, res) => {
+            let email = req.body.email;
+            let username = req.body.username;
+            let password_confirmation = req.body.password_confirmation;
+
+            username = username.toLowerCase();
+
+            if (username.length > 12 || password_confirmation.length > 12) {
+                res.json({
+                    success: false,
+                    msg: 'Nazwa użytkownika lub hasło przekracza dozwoloną ilość znaków (12)'
+                })
+                return;
+            }
+
+            let checkUsername = [username];
+            db.query('SELECT * FROM user WHERE username = ? LIMIT 1', checkUsername, (err, data, fields) => {
+                if(err) {
+                    res.json({
+                        success:false,
+                        msg: 'Something went wrong with registration'
+                    })
+                    return;
+                } else if(data && data.length === 0) {
+                    let hashPswd = password_confirmation;
+                    const bcrypt = require('bcrypt');
+
+                    let pswd = bcrypt.hashSync(password_confirmation, 9);
+
+
+
+                    let values = [
+                        [null, username, pswd]
+                    ];
+
+                    db.query('INSERT INTO `user` (`id`, `username`, `password`) VALUES ?', [values], (err, data, fields) => {
+                        if(err) {
+                            console.log('Register error');
+                        } else {
+                            res.json({
+                                success: true,
+                                msg: 'Użytkownik został zarejestronwany'
+                            })
+                            return;
+                        }
+                    })
+                    
+                } else if(data && data.length === 1) {
+                    res.json({
+                        success:false,
+                        msg: 'Użytkownik o danej nazwie użytkownika już istnieje'
+                    })
+                    return;
+                }
+            })
+
+        });
+    }
+
+    login(app, db) {
+        
+        app.post('/login', (req, res) => {
+            let username = req.body.username;
+            let password = req.body.password;
+
+            username = username.toLowerCase();
+
+            if(username.length > 12 || password.length > 12) {
+                res.json({
+                    success: false,
+                    msg: 'Password or login too long'
+                })
+                return; 
+            }
+
+            let checkUser = [username];
+            db.query('SELECT * FROM user WHERE username = ? LIMIT 1', checkUser, (err, data, fields) => {
+                if(err) {
+                    res.json({
+                        success: false,
+                        msg: "Error - No such user in db"
+                    })
+                    return;
+                }
+
+                if(data && data.length === 1) {
+                    bcrypt.compare(password, data[0].password, (bcryptErr, verified) => {
+                        if(verified) {
+                            req.session.userID = data[0].id;
+
+                            res.json({
+                                success: true,
+                                username: data[0].username
+                            })
+
+                            console.log(data[0].username)
+                            return;
+                        } else {
+                            res.json({
+                                success: false,
+                                msg: 'Errorr'
+                            })
+                        }
+                    })
+                } else [
+                    res.json({
+                        success: false,
+                        msg: 'User not found'
+                    })
+                ]
+            });
+        });
+    }
+
+
+
+    logout(app, db) {
+        app.post('/logout', (req, res) => {
+            if(req.session.userID) {
+                req.session.destroy();
+                res.json({
+                    success: true
+                })
+
+                return true;
+            } else {
+                res.json({
+                    success:false
+                })
+                return false;
+            }
+        })
+    }
+
+    isLoggedIn(app, db) {
+        app.post('/isLoggedIn', (req, res) => {
+            if(req.session.userID) {
+                
+                let cols = [req.session.userID];
+                db.query('SELECT * FROM user WHERE id = ? LIMIT 1', cols, (err, data, fields) => {
+                    if(data && data.length === 1) {
+                        res.json({
+                            success: true,
+                            username: data[0].username
+                        }) 
+                        return true;
+                    } else {
+                        res.json({
+                            success: false,
+                            username: ''
+                        }) 
+                    }
+                });
+
+            }  else {
+                res.json({
+                    success: false
+                })
+            }
+        });
+    }
+}
+
+module.exports = Router;
---
+++ build/precache-manifest.26545d85419302c8a1af86734cee3653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda10aad62efaad73cde0b9849ce4fc1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "095b99cef961f870813d",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "81af8379f37e70dfb645",
+    "url": "./static/js/2.8b24554f.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "./static/js/2.8b24554f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "095b99cef961f870813d",
+    "url": "./static/js/main.d7291a33.chunk.js"
+  },
+  {
+    "revision": "a653ca34669b4c72661a",
+    "url": "./static/js/runtime-main.c3a93ba8.js"
+  }
+]);
